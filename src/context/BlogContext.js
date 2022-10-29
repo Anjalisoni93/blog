cbde6072be7c0106818661a67f27f5064b7cc8d3
Anjali Soni <anjalipatadiya@gmail.com>
2022-10-29 17:22:17 -0600

    Export BlogContext and add a value to the BlogProvider

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,8 +4,12 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>
-    {/* So children in this case is essentially an App component that we are passing down as a prop */}
+  return <BlogContext.Provider value={[1, 2, 3]}>
+    {/* So children in this case is 
+    essentially an App component that 
+    we are passing down as a prop */}
     {children}
   </BlogContext.Provider>
 };
+
+export default BlogContext;

f81f9210634473c767eebd32459ad0e1add68c9b
Anjali Soni <anjalipatadiya@gmail.com>
2022-10-30 14:30:31 -0600

    Export createDataContext and make use of that function by removing duplicate code

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,30 +1,21 @@
 import React, { useReducer } from "react";
-
-
-const BlogContext = React.createContext();
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }] ;
-    default: 
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
       return state;
   }
 };
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
-    dispatch({ type: 'add_blogpost' });
-  };
-
-  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-    {/* So children in this case is 
-    essentially an App component that 
-    we are passing down as a prop */}
-    {children}
-  </BlogContext.Provider>
+const addBlogPost = () => {
+  dispatch({ type: 'add_blogpost' });
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);

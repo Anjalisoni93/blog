dc2e91f119a85ef1df8fcc13de339523bca7e233
Anjali Soni <anjalipatadiya@gmail.com>
2022-10-31 17:43:23 -0600

    Add a new component called Show screen

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ShowScreen = () => {
+  return <View>
+    <Text>Show Screen</Text>
+  </View>
+};
+
+const style = StyleSheet.create({});
+
+export default ShowScreen;

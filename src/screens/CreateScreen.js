5c9f1ecfd1b020ec374878264f21329bfac7e2f2
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-02 16:12:02 -0600

    Add text input for title and content

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,22 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text>Enter Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text>Enter Content:</Text>
+      <TextInput 
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
     </View>
   )
 };

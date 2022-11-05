15a1f3877268efa1743b757f87e9d7fabae0a058
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-04 21:09:21 -0600

    Iterate through state to retrieve a specific id of the blogPost

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -5,6 +5,12 @@ import { Context } from "../context/BlogContext";
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
 
+  const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam('id')
+    );
+
+    const [title, setTitle] = useState();
+
   return (
     <View>
       <Text>Edit Screen - {navigation.getParam('id')}</Text>

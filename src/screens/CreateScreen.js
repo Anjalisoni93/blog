b6fce012dcb1c040dee627d6c311ba1ebdc835aa
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-04 23:18:12 -0600

    Remove StyleSheet import

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 

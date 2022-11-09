8ce57054a7f12c49a8e6efed3fab7ae267fcb6c3
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-09 14:55:29 -0700

    Remove extra space

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
   // Make sure to update the baseURL 
   // each time when you restart the tunnel 
   // as it changes each time
-  baseURL: ' https://b4c3-75-158-102-96.ngrok.io'
+  baseURL: 'https://b4c3-75-158-102-96.ngrok.io'
 });

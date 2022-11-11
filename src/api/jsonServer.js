ad854f5258c19c2379f33dfec784600e96f3f50f
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-11 15:25:39 -0700

    Update the tunnel baseURL for tunnel

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -4,5 +4,5 @@ export default axios.create({
   // Make sure to update the baseURL 
   // each time when you restart the tunnel 
   // as it changes each time
-  baseURL: 'https://b4c3-75-158-102-96.ngrok.io'
+  baseURL: 'https://488d-75-158-102-96.ngrok.io'
 });
